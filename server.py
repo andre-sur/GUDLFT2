43b519862991731431bfa355afec9f944db84340
andre <andresuruguepro@gmail.com>
2025-06-05 06:38:41 +0200

    ajout routage vers affichage des points

--- server.py
+++ server.py
@@ -57,3 +57,7 @@ def purchasePlaces():
 @app.route('/logout')
 def logout():
     return redirect(url_for('index'))
+
+@app.route('/points', methods=['GET'])
+def showPoints():
+    return render_template('points.html', clubs=clubs)
